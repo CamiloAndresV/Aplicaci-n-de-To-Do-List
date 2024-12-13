df863501da367b0296922038d4888645d9d652e8
camilo <ospinavillacamiloandres@gmail.com>
2024-12-13 14:13:13 -0500

    comments and menu

---
+++ To do list.py
@@ -0,0 +1,9 @@
+#codigo para la gestion de tareas diarias
+#se puede agregar mas funciones para la gestion de tareas
+def gestion_tareas():
+    print("Gestion de tareas diarias")
+    print("1. Agregar tarea")
+    print("2. Eliminar tarea")
+    print("3. Listar tareas")
+    print("4. Salir")
+opcion = input("Ingrese una opcion: ")
